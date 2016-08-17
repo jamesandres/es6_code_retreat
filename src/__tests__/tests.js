b71b47a1d83f065e03960f11a96a5674c7d180fc
James Andres <james.andres@gmail.com>
2016-08-17 13:20:46 +0100

    Werkin with sGareth

--- src/__tests__/tests.js
+++ src/__tests__/tests.js
@@ -1,13 +1,64 @@
 jest.unmock('../app');
 
 describe('initCells', () => {
-    it('has a cell alive', () => {
+
+    // it('loop called with correct values', () => {
+    //     const app = require('../app');
+    //     app.draw = jest.fn()
+
+    //     app.loop(1, 2)
+    //     expect(app.draw).toBecalledWith(1, 2)
+    // })
+
+    // it('nextState returns the next state .. obvs', () => {
+    //     const app = require('../app');
+    //     const cells = app.initCells();
+
+    //     let newCells = app.nextState(cells);
+
+    //     expect(newCells).toBe(
+    //         [[1, 0, 1],
+    //          [1, 1, 0],
+    //          [0, 0, 0]]
+    //     );
+    // })
+
+    it('countNeighbours returns the correct value', () => {
         const app = require('../app');
-        expect(app.initCells()[1][1]).toBe(1)
+
+        let cells = [[0, 1, 0],
+                     [0, 0, 1],
+                     [1, 1, 1]];
+        expect(app.countNeighbours(cells, 0, 0)).toBe(5)
+        expect(app.countNeighbours(cells, 0, 1)).toBe(4)
+        expect(app.countNeighbours(cells, 0, 2)).toBe(5)
+
+        expect(app.countNeighbours(cells, 1, 0)).toBe(5)
+        expect(app.countNeighbours(cells, 1, 1)).toBe(5)
+        expect(app.countNeighbours(cells, 1, 2)).toBe(4)
+
+        // expect(app.countNeighbours(cells, 1, 1)).toBe(5)
     })
 
-    it('has that same cell dead', () => {
+    it('isAlive returns true if cell should be alive, false otherwise', () => {
         const app = require('../app');
-        expect(app.initCells()[1][1]).toBe(0)
+        const cells = app.initCells();
+
+        expect(app.isAlive(0, 1)).toBe(0);
+        expect(app.isAlive(0, 2)).toBe(0);
+        expect(app.isAlive(0, 3)).toBe(1);
+        expect(app.isAlive(0, 4)).toBe(0);
+        expect(app.isAlive(0, 5)).toBe(0);
+        expect(app.isAlive(0, 6)).toBe(0);
+        expect(app.isAlive(0, 7)).toBe(0);
+        expect(app.isAlive(0, 8)).toBe(0);
+        expect(app.isAlive(1, 1)).toBe(0);
+        expect(app.isAlive(1, 2)).toBe(1);
+        expect(app.isAlive(1, 3)).toBe(1);
+        expect(app.isAlive(1, 4)).toBe(0);
+        expect(app.isAlive(1, 5)).toBe(0);
+        expect(app.isAlive(1, 6)).toBe(0);
+        expect(app.isAlive(1, 7)).toBe(0);
+        expect(app.isAlive(1, 8)).toBe(0);
     })
 })
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const cellSize = 15,
       cellsHoriz = 60,
-      cellsVert = 30
+      cellsVert = 30,
+      numRows = 3,
+      numCols = 3;
 
 function initCanvas() {
     let canvasEl = document.getElementById('canvas'),
@@ -16,9 +18,9 @@ function initCanvas() {
 }
 
 function initCells() {
-    return [[0, 0, 0],
-            [0, 1, 0],
-            [0, 0, 0]];
+    return [[0, 1, 0],
+            [0, 0, 1],
+            [1, 1, 1]];
 }
 
 function draw(cells, canvas) {
@@ -39,16 +41,87 @@ function draw(cells, canvas) {
             }
         });
     });
+    console.log("Drawing...");
 }
+
+function countNeighbours(cells, x, y) {
+    let numNeighbours = 0;
+
+    for (var dx = -1; dx <= 1; dx++) {
+        for (var dy = -1; dy <= 1; dy++) {
+            let i = (x + dx + numRows) % numRows,
+                j = (y + dy + numCols) % numCols;
+
+            if (i === x && j === y) {
+                continue;
+            }
+
+            numNeighbours += cells[i][j];
+        }
+    }
+
+    return numNeighbours;
+}
+
+
+function isAlive(cell, numNeighbours) {
+    switch(cell){
+        case 1:
+            if(numNeighbours < 2 || numNeighbours > 3) {
+                return 0
+            }
+            else {
+                return 1
+            }
+        case 0:
+            if(numNeighbours == 3) {
+                    return 1
+            }
+            else {
+                return 0
+            }
+    }
+    return 0;
+}
+
+
+function nextState(cells) {
+    let newCells = [];
+
+    cells.forEach((row, x) => {
+        newCells[x] = [];
+
+        row.forEach((cell, y) => {
+            let numNeighbours = countNeighbours(cells, x, y);
+
+            newCells[x][y] = isAlive(cell, numNeighbours);
+        });
+    });
+
+    return newCells;
+}
+
+function loop(cells, canvas) {
+    cells = nextState(cells);
+
+    draw(cells, canvas);
+}
+
 
 function run() {
     let cells = initCells(),
         canvas = initCanvas();
-    draw(cells, canvas)
+
+    setInterval(function () {loop(cells, canvas)}, 1000);
 }
 
 window.run = run;
 
 module.exports = {
-    initCells: initCells
+    initCells: initCells,
+    draw: draw,
+    countNeighbours: countNeighbours,
+    isAlive: isAlive,
+    nextState: nextState,
+    loop: loop,
 };
